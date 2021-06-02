811af9342b800cfe0f4f222728f6131eca986d88
Dylan Medina <dlondonmedina@gmail.com>
2021-06-02 13:23:40 -0700

    loan functionality added to models

--- app/models.py
+++ app/models.py
@@ -1,30 +1,43 @@
 from app import db 
 
+loans = db.Table('loans',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'))
+)
+
+
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    loans = db.relationship('Loan', backref='loan', lazy='dynamic')
-
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 
 class Book(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), index=True, unique=True)
     author = db.Column(db.String(120))
     genre = db.Column(db.String(64)) # literary, scifi, romance, fantasy, western, children
-    borrowers = db.relationship('Loan', backref='borrower', lazy='dynamic')
+    borrowers = db.relationship('User', secondary=loans, backref='books')
 
     def __repr__(self):
         return '<Book {}>'.format(self.title)
 
-
-class Loan(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    active = db.Column(db.Boolean(), default=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    def checked_out(self):
+        return self.borrowers
+    
+    def checkout(self, user):
+        if not self.checked_out():
+            self.borrowers.append(user)
+        
+    
+    def checkin(self, user):
+        if user in self.borrowers:
+            self.borrowers.remove(user)
+    
+    
